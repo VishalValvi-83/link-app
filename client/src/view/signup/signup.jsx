841050d445811e4163f14f9c254a2b1d636532cc
Phobial <harshalpatilcsn2020@gmail.com>
2025-04-06 18:07:54 +0530

    some changes in login ans signup pages did

--- client/src/view/signup/signup.jsx
+++ client/src/view/signup/signup.jsx
@@ -136,8 +136,8 @@ const Signup = () => {
             <div className="relative">
               <div className="w-full max-w-xl xl:w-full xl:mx-auto xl:pr-24 xl:max-w-xl">
                 <h3 className="text-4xl font-bold text-white">
-                  “Join a growing community  <br className="hidden xl:block" />
-                  & Simplify your links"
+                  Join a growing community  <br className="hidden xl:block" />
+                  & Simplify your links!
                 </h3>
                 <ul className="grid grid-cols-1 mt-10 sm:grid-cols-2 gap-x-8 gap-y-4">
                   <li className="flex items-center space-x-3">

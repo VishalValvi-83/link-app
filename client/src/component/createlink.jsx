e32608439ab169c61867f8efd03a1c009b4744fa
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-04-07 21:10:39 +0530

    create link form changed

--- client/src/component/createlink.jsx
+++ client/src/component/createlink.jsx
@@ -3,6 +3,8 @@ import './style.css'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import Navbarnew from './Navb'
+import { Link } from 'lucide-react';
+
 
 const Createlink = () => {
     const [linkData, setLinkData] = useState({
@@ -134,38 +136,10 @@ const Createlink = () => {
                     <a href='#createlink' className="flex w-32 justify-center mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 text-center px-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">Lets Make</a>
                 </div>
             </section>
-            <section id='createlink' className='relative wave-container w-full'>
-                <div className="container z-50 lg:w-3/4  mx-auto ">
-                    <div className="p-6 rounded shadow-md">
-                        <div>
-                            <div className="mb-4">
-                                <label className="block pb-2 font-medium text-white">Title</label>
-                                <input value={linkData.title} onChange={(e) => { setLinkData({ ...linkData, title: e.target.value }) }} type="text" placeholder="Title for short Link" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-indigo-200" required />
-                            </div>
-
-                            <div className="mb-4">
-                                <label className="block pb-2 font-medium text-white">Slug</label>
-                                <input value={linkData.slug} onChange={(e) => { setLinkData({ ...linkData, slug: e.target.value }) }} type="text" placeholder="e.g., my_short_link or myapplink" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-indigo-200" required />
-                            </div>
-
-                            <div className="flex items-center mb-4">
-                                <p className="py-2.5 px-3 text-gray-500 bg-white  border border-r-0 rtl:rounded-r-lg rtl:rounded-l-none rtl:border-l-0 rtl:border-r rounded-l-lg">https://</p>
-                                <input value={linkData.target} onChange={(e) => { setLinkData({ ...linkData, target: e.target.value }) }} type="text" placeholder="Paste your long URL" className="block w-full rounded-l-none rtl:rounded-l-lg rtl:rounded-r-none  rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 " required />
-                            </div>
-
-                            <button onClick={getData} type="submit" className="px-4 py-2 font-bold text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-300">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </section>
             <section id="createlink" className="relative bg-gray-100 dark:bg-gray-900 py-16 px-4">
                 <div className="container mx-auto max-w-2xl bg-white dark:bg-gray-800 rounded-2xl shadow-lg p-8">
-                    <h2 className="text-2xl font-bold text-center text-gray-800 dark:text-white mb-6">Create Your Short Link</h2>
+                    <h2 className="text-2xl inline-flex items-center justify-center gap-3 mx-auto font-bold text-purple-500 mb-6"><Link /> Create Your Short Link</h2>
                     <form onSubmit={getData} className="space-y-6">
-
-                        {/* Title Input */}
                         <div>
                             <label className="block sm:text-lg text-gray-700 dark:text-gray-300 mb-1">Title</label>
                             <input
@@ -173,10 +147,8 @@ const Createlink = () => {
                                 value={linkData.title}
                                 onChange={(e) => setLinkData({ ...linkData, title: e.target.value })}
                                 placeholder="Title for the short link"
-                                className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            />
+                                className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" />
                         </div>
-                        {/* Slug Input */}
                         <div>
                             <label className="block sm:text-lg text-gray-700 dark:text-gray-300 mb-1">Slug</label>
                             <input
@@ -187,13 +159,10 @@ const Createlink = () => {
                                 className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             />
                         </div>
-                        {/* URL Input */}
                         <div>
                             <label className="block sm:text-lg text-gray-700 dark:text-gray-300 mb-1">Long URL</label>
                             <div className="flex">
-                                <span className="inline-flex items-center px-2 bg-gray-200 text-gray-600 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-700 dark:text-white">
-                                    https://
-                                </span>
+                                <span className="inline-flex items-center px-2 bg-gray-200 text-gray-600 rounded-l-md border border-r-0 border-gray-300 dark:bg-gray-700 dark:text-white">https://</span>
                                 <input
                                     type="text"
                                     value={linkData.target}
@@ -203,86 +172,17 @@ const Createlink = () => {
                                 />
                             </div>
                         </div>
-
-                        {/* Submit Button */}
                         <div className="text-center">
                             <button
                                 type="submit"
                                 className="w-full md:w-auto px-8 py-3 bg-indigo-600 hover:bg-indigo-700 text-white rounded-lg font-semibold transition duration-300"
-                            >
-                                Generate Short Link
+                            >Generate Short Link
                             </button>
                         </div>
                     </form>
                 </div>
             </section>
-            {/* <section className="text-gray-600 body-font">
-                <div className="container px-5 my-5 mx-auto">
-                    <div className="text-center mb-10">
-                        <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-purple-500 mb-4">FEATURES</h1>
-                        <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Create Smarter Links, Drive Better Results</h1>
-                        <p className="text-base xl:w-2/4 lg:w-3/4 mx-auto">Transform long, messy URLs into concise, branded links that are easy to share and track. Elevate your marketing campaigns with detailed analytics, custom slugs, and secure link-sharing options designed for success.</p>
-                    </div>
-                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">QR Code Generation</span>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">Click Tracking & Analytics</span>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">Custom Domain Support</span>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">Customizable Slugs</span>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">Spam & Malware Detection
-                                </span>
-                            </div>
-                        </div>
-                        <div className="p-2 sm:w-1/2 w-full">
-                            <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                    <path d="M22 4L12 14.01l-3-3"></path>
-                                </svg>
-                                <span className="title-font font-medium">Social Media Integration</span>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="flex mx-auto mt-16 text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">Try Now</button>
-                </div>
-            </section> */}
+
         </>
     )
 }

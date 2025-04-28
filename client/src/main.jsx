6e391796ef6611748fa9663917c8fe5b0abb00c1
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-04-28 14:40:51 +0530

    fixed path of login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import About from "./view/About/About";
 import Contact from "./view/Contact/Contact";
 import './index.css'
 import React from "react";
-import Signin from "./view/Login/login";
+import Signin from "./view/Login/Login";
 import Signup from "./view/signup/signup";
 import Error from "./component/404";
 import Createlink from "./component/createlink";

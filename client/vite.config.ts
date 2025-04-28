f57ddeb81d40e62e155261fc8053f81561c05776
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-04-28 13:59:45 +0530

    fix: cleaned node_modules and locked versions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',    
+    sourcemap: false,  
+    minify: 'esbuild', 
+    target: 'esnext'
+  }
 });

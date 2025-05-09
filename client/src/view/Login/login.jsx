0a001e831387301da5a55200c916b58a46306d5f
Phobial <harshalpatilcsn2020@gmail.com>
2025-05-09 10:50:06 +0530

    conflict resolved

--- client/src/view/Login/login.jsx
+++ client/src/view/Login/login.jsx
@@ -168,8 +168,8 @@ export default function Signin() {
   return (
     <>
       <Navbarnew user={user} />
-      <section className="bg-white">
-        <div className="grid h-screen grid-cols-1 lg:grid-cols-2">
+      <section className="bg-white md:mt-16">
+        <div className="grid grid-cols-1 md:h-screen lg:grid-cols-2">
           <div className="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
             <div className="absolute inset-0">
               <img

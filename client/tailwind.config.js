553dcbf5bbcf0ec3423174641568cffb7c8bc524
VishalValvi-83 <vishalvalvi2529@gmail.com>
2025-05-15 00:10:59 +0530

    feat: add @react-oauth/google and jwt-decode dependencies; update Tailwind and Vite configurations to include flowbite-react

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,17 @@
+const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
       "./node_modules/flowbite/**/*.js",
+      ".flowbite-react\\class-list.json"
     ],
     theme: {
       extend: {},
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
+    plugins: [require('flowbite/plugin'), flowbiteReact],
     darkMode: 'class', 
   };
   
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite';
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 
 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   build: {
     outDir: 'dist',    
     sourcemap: false,  
